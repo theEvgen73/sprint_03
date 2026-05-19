5349a1c763951759a23e37c5aae635d79af83f8b
Evgenii <e.paprotsky@gmail.com>
2026-05-19 18:25:07 +0400

    Обновлён сториборд и ViewController

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter = 0 {
+        didSet {
+            counterLabel.text = "Значение счетчика: \(counter)"
+        }
+    }
+    
+    
+    @IBAction func incrementButtonTapped(_ sender: Any) {
+        counter += 1
+    }
+    @IBAction func subtractionButtonTapped(_ sender: Any) {
+        guard counter > 0 else{
+            return
+        }
+        counter -= 1
+
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        counter = 0
+    }
+    
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 0
     }
 
 
